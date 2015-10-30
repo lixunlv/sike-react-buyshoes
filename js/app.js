f7e9857c8afd64175bf2f6a9e6bd48e6c47e094b
lixunlv <lxunlv@163.com>
2015-10-30 12:36:34 +0800

    2

--- js/app.js
+++ js/app.js
@@ -1,6 +1,11 @@
 window.onload = function() {
   console.log("page loaded");
   makeCartScrollNicely();
+
+  var $toggle = document.querySelector(".site__right-sidebar-toggle");
+  $toggle.addEventListener("click",function() {
+    document.body.classList.toggle("js-show-right-sidebar");
+  });
 }
 
 function makeCartScrollNicely() {
